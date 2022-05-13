ee221e813fcb787b0fd0aca58273f452994692a7
luis1310 <ladelacruzm@uni.pe>
2022-05-12 21:29:27 -0500

    Creacion de la logica del juego
    Primera versión de la logica del juego

---
+++ NTwoM/src/sprint1_0/Product/GameException.java
@@ -0,0 +1,9 @@
+package sprint1_0.Product;
+
+public class GameException extends Throwable {
+    private static final long serialVersionUID = -4036588148354448335L;
+
+    public GameException(String string) {
+        super(string);
+    }
+}
---
+++ NTwoM/src/sprint1_0/Product/Main.java
@@ -0,0 +1,5 @@
+package sprint1_0.Product;
+
+public class Main {
+
+}
