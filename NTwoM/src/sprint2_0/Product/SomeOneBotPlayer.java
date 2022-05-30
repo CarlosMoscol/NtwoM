5fed07e3efd0dbade3ad45669e778b0bd330d599
luis1310 <43614832+luis1310@users.noreply.github.com>
2022-05-30 01:56:19 -0500

    actualización some bot player

--- NTwoM/src/sprint2_0/Product/SomeOneBotPlayer.java
+++ NTwoM/src/sprint2_0/Product/SomeOneBotPlayer.java
@@ -19,6 +19,23 @@ public class SomeOneBotPlayer extends BotPlayer{
 			}
 		}
 	}
+	
+	@Override
+	public int getIndexToRemovePieceOfOpponent(Board_GUI gameBoard) {
+		while(true) {
+			try {
+				int index = rand.nextInt(Board_GUI.NUM_POSITIONS_OF_BOARD);
+				Token playerOccupying = gameBoard.getPosition(index).getPlayerOccupyingIt();
+				if(playerOccupying != Token.NO_PLAYER && playerOccupying != this.playerToken) {
+					return index;
+				}
+			} catch (GameException e) {
+				e.printStackTrace();
+			}
+		}
+	}	
+	
+	
 
 }
 
